d181f0347491bea499a9380bccd1ff5fe2b09c09
Noelle Laureano <nrlaureano@gmail.com>
2018-05-31 14:34:25 -0400

    file reorg; test to deploy

--- src/layouts/components/Projects.js
+++ src/layouts/components/Projects.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import styles from './Projects.module.css'
-import Container from '../components/container'
+import Container from './'
 
 import { mapStackMockup, saucesomeMockup, theListeningRoomMockup } from '../images/projects'
 
