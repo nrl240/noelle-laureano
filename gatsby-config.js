5eab5e6ee2f461e1326de64fbea9b45715f24935
Noelle Laureano <nrlaureano@gmail.com>
2018-06-10 22:04:00 -0400

    adds favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,26 @@ module.exports = {
   siteMetadata: {
     title: 'Noelle Laureano',
   },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-netlify'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-netlify',
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/favicon.png",
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: false
+        }
+      }
+    }
+  ],
 }
